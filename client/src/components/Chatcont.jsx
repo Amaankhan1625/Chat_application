37f3c4c8851e5840a9060f37fb1dc3739cfca358
Amaankhan1625 <amaankhanjatu16@gmail.com>
2026-08-13 11:09:06 +0530

    fourth commit

--- client/src/components/Chatcont.jsx
+++ client/src/components/Chatcont.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import assets from '../assets/assets.js'
 
-const Chatcont = () => {
-  return (
-    <div>
-      <h1>chat box</h1>
+const Chatcont = ({selectedUser , setSelectedUser}) => {
+  return selectedUser ?  (
+    <div className = 'h-full overflow-scroll relative blackdrop-blur-lg'>
+      <div className = "flex items-centre gap-3 py-4 px-5 border-b border-stone-600"> 
+             <img src = {assets.profile_martin} alt = "profile" className = "w-8 rounded-full"/>
+             
+                <p className = "flex-1 text-lg text-white flex item-centre gap-2">
+                  Martin Johnson
+                  <span className = "text-green-400 rounded-full w-2 h-2">Online</span>
+                </p>
+                <img onClick={()=> setSelectedUser(null)} src = {assets.arrow_icon} alt = "arrow" className = "md:hidden max-w-7"/>
+                <img src = {assets.help_icon} alt = "help" className = "max-md:hidden max-w-5"/>
+      </div>
+    </div>  
+  ) : (
+    <div className = 'flex flex-col justify-center items-center h-full gap-2 text-gray-500 bg-white/10 max-md:hidden'>
+      <img src = {assets.logo.icon} className='max-w-16' alt="" />
+      <p className = 'text-white font-medium text-lg'>CHAT HERE</p>
     </div>
   )
 }
--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -10,14 +10,14 @@ const Homepage = () => {
 
   return (
 
-<div className =  'border w-full h-screen sm:px-[15%] sm:py-[5%]'> 
-  <div className={`backdrop-blur-xl border-2 border-gray-600 rounded-2x1 overflow-hidden h-[100%] grid grid-cols-1 relative ${selectedUser ?
-'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]' : ''}'`}>
-    <Sidebar />
-    <Chatcont/>
+ <div className =  'border w-full h-screen sm:px-[15%] sm:py-[5%]'> 
+  <div className={`backdrop-blur-xl border-2 border-gray-600 rounded-2x1 overflow-hidden h-fullgrid grid-cols-1 relative ${selectedUser ?'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]' : 'md:grid-cols-2'}'`}>
+    <Sidebar selectedUser = {selectedUser} setSelectedUser = {setSelectuser}/>
+    <Chatcont selectedUser = {selectedUser} setSelectedUser = {setSelectuser}/>
     <Rightslide />
   </div>
 </div>
+
 
   )
 }
