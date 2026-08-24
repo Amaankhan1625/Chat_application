dd69a465f3931ad462b29b4490cc35e3b7f1189f
Amaankhan1625 <amaankhanjatu16@gmail.com>
2026-08-24 13:27:30 +0530

    redirection to login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
-import {Route,Routes} from 'react-router-dom' 
+import { useContext } from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
 import Homepage from './pages/Homepage'
 import Loginpage from './pages/Loginpage'
 import Profilepage from './pages/Profilepage'
-
+import {Toaster} from 'react-hot-toast'
+import {AuthContext} from '../context/Authcontext.jsx'
 const App = () => { 
+  const {authUser} = useContext(AuthContext) ;  
   return (
     <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain">
+      <Toaster/>
       <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/Login" element={<Loginpage />} />
-        <Route path="/profile" element={<Profilepage />} />
+        <Route path="/" element={authUser? <Homepage/>:<Navigate to="/Login" />} />
+        <Route path="/Login" element={!authUser?<Loginpage/>:<Navigate to="/" />} />
+        <Route path="/profile" element={authUser?<Profilepage/>:<Navigate to="/Login" />} />
       </Routes>
     </div>
   )
